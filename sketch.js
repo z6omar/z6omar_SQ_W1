5c7826d57992577bc98ed2598fd04add3b2abc4f
Zahra <z6omar@uwaterloo.ca>
2026-05-13 14:21:50 -0400

    changed image name

--- sketch.js
+++ sketch.js
@@ -1,9 +1,7 @@
-// image, background colour, text atleast, shape . has to be jenga or exit 8. make sure to cite the image, hiuman descions with ss, upload the process file as a pdf with repo and live link look at guide line .
-
 let img;
 
 function preload() {
-  img = loadImage('exit_8_game_image.jpg');
+  img = loadImage("assets/images/exit_8_game_image.jpg");
 }
 
 function setup() {
@@ -13,34 +11,34 @@ function setup() {
 }
 
 function draw() {
-  // Light grey background
   background(200);
 
-  // Draw image in center
   let imgX = width / 2;
   let imgY = height / 2;
-  imageMode(CENTER);
-  image(img, imgX, imgY);
 
-  // Rectangle settings
+  imageMode(CENTER);
+
+  // Draw image safely sized
+  if (img) {
+    image(img, imgX, imgY, 400, 300);
+  } else {
+    fill(0);
+    text("Image not loaded", width/2, height/2);
+  }
+
   let rectWidth = 180;
   let rectHeight = 40;
-  let spacing = 60;
+  let spacing = 70;
 
-  fill(0); // black rectangles
+  fill(0);
   noStroke();
 
-  // Start button rectangle
   rect(imgX, imgY - spacing / 2, rectWidth, rectHeight);
-
-  // Instructions button rectangle
   rect(imgX, imgY + spacing / 2, rectWidth, rectHeight);
 
-  // White text
   fill(255);
   textSize(16);
 
   text("start", imgX, imgY - spacing / 2);
   text("instructions", imgX, imgY + spacing / 2);
 }
-``
